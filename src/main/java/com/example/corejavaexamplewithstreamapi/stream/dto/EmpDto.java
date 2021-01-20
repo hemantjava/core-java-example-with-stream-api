9caffc1e6e8046ce03cc969e0b37d6a5dfabee0c
hemantjava <hemantjava90@gmail.com>
2021-01-20 19:02:58 +0530

    delegate pattern final update.

--- src/main/java/com/example/corejavaexamplewithstreamapi/stream/dto/EmpDto.java
+++ src/main/java/com/example/corejavaexamplewithstreamapi/stream/dto/EmpDto.java
@@ -1,28 +1,31 @@
 package com.example.corejavaexamplewithstreamapi.stream.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @ToString
-@RequiredArgsConstructor
-@AllArgsConstructor
 @Builder
 public class EmpDto {
 
     private String name;
     private Integer id;
 
-    public static List<EmpDto> getEmp (List<Emp> empList) {
-        return empList.stream().map(EmpDto::new).collect(Collectors.toList());
-    }
 
     @ToString.Exclude
     private final Emp emp;
+
+    public EmpDto (Emp emp) {
+        this.emp = emp;
+    }
+
+    public EmpDto (String name, Integer id, Emp emp) {
+        this.name = name;
+        this.id = id;
+        this.emp = emp;
+    }
 
     public String getName ( ) {
         return this.emp.getName();
@@ -30,6 +33,10 @@ public class EmpDto {
 
     public Integer getId ( ) {
         return this.emp.getId();
+    }
+
+    public static List<EmpDto> getEmp (List<Emp> empList) {
+        return empList.stream().map(EmpDto::new).collect(Collectors.toList());
     }
 
 

dd784ebd5e88edb7fd04c0b92c6fc29955f120ba
hemant sahu <hemantjava90@gmail.com>
2021-02-24 00:34:22 +0530

    Bubble sort

--- src/main/java/com/example/corejavaexamplewithstreamapi/sorting/BubbleSort.java
+++ src/main/java/com/example/corejavaexamplewithstreamapi/sorting/BubbleSort.java
@@ -6,11 +6,15 @@ public class BubbleSort {
         //int temp;
         for (int i = 0; i < arr.length; i++) {
       //arr.length-i-1 first iteration max value at the end
-            for (int j = 0; j < arr.length - i - 1; j++) {
+            for (int j = 0; j < arr.length - i -1; j++) {
                 if (arr[j] > arr[j + 1]) {
-                   int temp = arr[j];
+                   /*int temp = arr[j];
                     arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
+                    arr[j + 1] = temp;*/
+                    //without using 3rd variables
+                    arr[j] = arr[j] + arr[j+1];
+                    arr[j+1] = arr[j] -arr[j+1];
+                    arr[j] = arr[j] -arr[j+1];
                 }
             }
         }
